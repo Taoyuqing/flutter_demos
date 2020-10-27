c4c32c66e37e0581e7f4c10059e7d2e19f6d3eae
Taoyuqing <690230951@qq.com.com>
2020-10-27 14:37:19 +0800

    去掉多余的代码

--- lib/redux/reducer.dart
+++ lib/redux/reducer.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_widget_use/redux/states.dart';
 
 // 创建reducer
-enum Types { action1, action2, changeTheme, changeLanguage }
+enum Types { action1, action2, changeTheme }
 IState rootReducer(IState state, dynamic action) {
   if (action['type'] == Types.action1) {
     state.index.count += action['payload'];
@@ -14,9 +14,6 @@ IState rootReducer(IState state, dynamic action) {
     if (action['payload'] is MaterialColor) {
       state.themeColor.primarySwatch = action['payload'];
     }
-  }
-  if (action['type'] == Types.changeLanguage) {
-    if (action['payload']) {}
   }
   return state;
 }
